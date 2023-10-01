5b404b3bdeeb9211cb2ac48c489705553e6edbed
MykytaPimonov <145226647+MykytaPimonov@users.noreply.github.com>
2023-10-01 16:01:38 +0300

    init

---
+++ task1/pt1_1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+Regex regex = new Regex("^[a-zA-Z][a-zA-Z\\d]{1,9}$");
+
+String line;
+while ((line = Console.ReadLine()) != null && !line.Equals(""))
+{
+    line = line.Trim();
+    Console.WriteLine(regex.IsMatch(line));
+}
---
+++ task2/pt1_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+String Filter(String line, String oldPart, String newPart)
+{
+    Regex regex = new Regex(oldPart, RegexOptions.IgnoreCase);
+    return regex.Replace(line, newPart);
+}
+
+String o = "firstname", n = "lastname";
+
+String input;
+while ((input = Console.ReadLine()) != null)
+{
+    if (input.Equals("")) break;
+
+    String res = Filter(input, o, n);
+    Console.WriteLine(input + "  ->  " + res);
+}
